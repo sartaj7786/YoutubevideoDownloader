351ceaa1eea681884bfd234ff82e6c356d9d2ce7
sartaj7786 <123380032+sartaj7786@users.noreply.github.com>
2023-01-23 21:54:38 +0530

    Add files via upload

---
+++ UtubeDownldr.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+
+def Download(Link):
+    youtubeObject=YouTube(link)
+    youtubeObject=youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("There has been an error in downloading your youtube video")
+        print("This download has completed! Yahoooo!")
+link=input("Put your youtube link here!! URL:")
+Download(link)
